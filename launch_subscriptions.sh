6b2bf0eb397cfe1904c3ebc952f21f200f482590
keripukki <arttu.kosonen@wsp.com>
2020-01-15 17:44:31 +0200

    Add subscription launcher

---
+++ launch_subscriptions.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Batch launch subscribe.py jobs in background.
+set -o errexit
+cd $HFPV2_ROOTDIR/subscriber
+source env/bin/activate
+# The following loop reads one line at a time from subscriber/jobs.txt
+# and launches a subscription process for each line.
+# There must be exactly 3 fields: topic, fields and duration,
+# and they must be separated by ";".
+# Note that the "&" after the python call leaves the process in background
+# and lets the loop go to next step immediately.
+IFS=";"
+while read topic fields duration; do
+  [[ $topic = \#* ]] || [[ -z $topic ]] && continue # Skip commented or empty lines
+  python subscribe.py --topic "$topic" --fields "$fields" --duration "$duration" &
+done<../subscriptions.txt
