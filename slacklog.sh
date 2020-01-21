0986414da0df535e3681cc67dcceb2b4dc405f9b
keripukki <arttu.kosonen@gmail.com>
2020-01-21 15:58:08 +0200

    Slacklog script WIP

--- slacklog.sh
+++ slacklog.sh
@@ -2,14 +2,14 @@
 # Send messages to a Slack channel.
 # `HFPV2_SLACK_WEBHOOK_URL`, `HFPV2_SLACK_CHANNEL` and `HFPV2_SLACK_USERNAME`
 # MUST be defined in `.env` for this to work.
+#
+# NOTE: Does not work yet
 envpath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )""/.env"
-[[ -f "$envpath" ]] && source "$envpath" || exit 1 ".env file not found"
-slacklog(){
-  [[ -n "$HFPV2_SLACK_WEBHOOK_URL" ]] || echo "slacklog.sh: HFPV2_SLACK_WEBHOOK_URL missing from .env" && exit 1
-  [[ -n "$HFPV2_SLACK_CHANNEL" ]] || echo "slacklog.sh: HFPV2_SLACK_CHANNEL missing from .env" && exit 1
-  [[ -n "$HFPV2_SLACK_USERNAME" ]] || echo "slacklog.sh: HFPV2_SLACK_USERNAME missing from .env" && exit 1
-  curl -X POST --data-urlencode \
-  "payload={\"channel\": \"$HFPV2_SLACK_CHANNEL\", \"username\": \"$HFPV2_SLACK_USERNAME\", \"text\": \"$1\"}" \
-  "$HFPV2_SLACK_WEBHOOK_URL"
-  echo ""
-}
+[[ -f "$envpath" ]] && source "$envpath" || echo ".env file not found"
+[[ -n "$HFPV2_SLACK_WEBHOOK_URL" ]] || echo "slacklog.sh: HFPV2_SLACK_WEBHOOK_URL missing from .env"
+[[ -n "$HFPV2_SLACK_CHANNEL" ]] || echo "slacklog.sh: HFPV2_SLACK_CHANNEL missing from .env"
+[[ -n "$HFPV2_SLACK_USERNAME" ]] || echo "slacklog.sh: HFPV2_SLACK_USERNAME missing from .env"
+curl -s -X POST --data-urlencode \
+"payload={\"channel\": \"$HFPV2_SLACK_CHANNEL\", \"username\": \"$HFPV2_SLACK_USERNAME\", \"text\": \"$1\"}" \
+"$HFPV2_SLACK_WEBHOOK_URL" > /dev/null
+echo ""
