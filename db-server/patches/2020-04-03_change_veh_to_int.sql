88a5aa2ca9a3b16c2b7d4edd233cd05b73c4d33d
keripukki <arttu.kosonen@gmail.com>
2020-06-05 11:04:43 +0300

    Fix veh column type
    Constraints cannot be deferred because then the changes would not 
    propagete to hypertable partitions. In addition, transaction block is 
    not needed here actually.

--- db-server/patches/2020-04-03_change_veh_to_int.sql
+++ db-server/patches/2020-04-03_change_veh_to_int.sql
@@ -3,13 +3,9 @@
  * NOTE: This does not seem to work with fully populated database
  *       due to memory / disk space issues.
  *
- * Arttu K 2020-04-03
+ * Arttu K 2020-04-03, 2020-06-05
  */
 \set ON_ERROR_STOP on
-
-BEGIN;
-
-SET CONSTRAINTS ALL DEFERRED;
 
 ALTER TABLE bus ALTER COLUMN veh SET DATA TYPE integer;
 ALTER TABLE tl_bus ALTER COLUMN veh SET DATA TYPE integer;
@@ -17,5 +13,3 @@ ALTER TABLE tram ALTER COLUMN veh SET DATA TYPE integer;
 ALTER TABLE tl_tram ALTER COLUMN veh SET DATA TYPE integer;
 ALTER TABLE metro ALTER COLUMN veh SET DATA TYPE integer;
 ALTER TABLE train ALTER COLUMN veh SET DATA TYPE integer;
-
-COMMIT;
